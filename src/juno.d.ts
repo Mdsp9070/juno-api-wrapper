ab6159d22a68e96b6ab59a1d9a0e7c141ece1ee5
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2020-09-02 20:46:45 -0300

    charges and balance

--- src/juno.d.ts
+++ src/juno.d.ts
@@ -20,11 +20,45 @@ declare namespace Juno {
       banks: {
         number: string;
         name: string;
-      }[]
-    }
+      }[];
+    };
+  }
+
+  export interface Charges {
+    _embedded: {
+      charges: {
+        id: string;
+        code: number;
+        dueDate: string | Date;
+      }[];
+    };
+  }
+
+  export interface Balance {
+    balance: number;
+    withheldBalance: number;
+    transferableBalance: number;
+  }
+
+  export interface Charge {
+    description: string;
+    amount: string;
+    installments: number;
+  }
+
+  export interface Billing {
+    name: string;
+    document: string;
+    email: string;
+    phone: string;
   }
 }
 
 declare namespace Returns {
-  export type Banks = { name: string, number: string;}[]
+  export type Banks = { name: string; number: string }[];
+  export type Charges = {
+    id: string;
+    code: number;
+    dueDate: string | Date;
+  }[];
 }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,65 @@
+import pkg from '../package.json';
 import axios, { AxiosInstance } from 'axios';
 import { AppError } from './errors/AppError';
 
 export class Juno {
-  api: AxiosInstance;
+  private api: AxiosInstance;
+  private headers: { [key: string]: string | number };
 
   constructor(private options: Juno.Options) {
     this.api = axios.create({
       baseURL: this.options.isSandbox
-        ? 'https://sandbox.boletobancario.com'
+        ? 'https://sandbox.boletobancario.com/api-integration'
         : 'https://api.juno.com.br',
     });
+
+    this.headers = {
+      'User-Agent': `${pkg.name}/${pkg.version}`,
+      'X-Api-Version': 2,
+      'X-Resource-Token': this.options.resourceToken,
+      Authorization: `Bearer ${this.options.accessToken}`,
+    };
   }
 
   async banks(): Promise<Returns.Banks> {
-    const { data, status } = await this.api.get<Juno.Banks>('/data/banks');
+    const { data, status } = await this.api.get<Juno.Banks>('/data/banks', {
+      headers: this.headers,
+    });
 
-    if (status !== 200) throw new AppError('Cannot get bank list', status)
+    if (status !== 200 || !data._embedded.banks.length)
+      throw new AppError('Cannot get bank list', status);
 
-    return data._embedded.banks
+    return data._embedded.banks;
+  }
+
+  async balance(): Promise<Juno.Balance> {
+    const { data, status } = await this.api.get<Juno.Balance>('/balance', {
+      headers: this.headers,
+    });
+
+    if (status !== 200 || !data.balance)
+      throw new AppError('Cannot get Balance', status);
+
+    return data;
+  }
+
+  async charge(
+    chargeData: Juno.Charge,
+    userData: Juno.Billing
+  ): Promise<Returns.Charges> {
+    const body = {
+      ...chargeData,
+      ...userData,
+    };
+
+    const { data, status } = await this.api.post<Juno.Charges>(
+      '/charges',
+      body
+    );
+
+    if (status !== 200 || !data._embedded.charges.length)
+      throw new AppError('Cannot create Charge', status);
+
+    return data._embedded.charges;
   }
 }
