d7b5477f1703787060a45b310df77e511c954410
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2020-09-26 19:38:41 -0300

    changed content-type

--- src/modules/authorization.ts
+++ src/modules/authorization.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Authorization } from '../@types/juno';
 import { AppError } from '../errors/AppError';
 import { getClientHash } from '../utils/utils';
 
@@ -9,15 +10,20 @@ export async function getAccessToken(
 ): Promise<string> {
   const hash = getClientHash(clientId, clientSecret);
 
-  const { data, status } = await axios({
-    url: isSandbox
-      ? 'https://sandbox.boletobancario.com/authorization-server/oauth/token'
-      : 'https://api.juno.com.br/authorization-server/oauth/token',
-    method: 'post',
-    headers: {
-      Authorization: `Basic: ${hash}`,
-    },
-  });
+  const { data, status } = await axios.post<Authorization>(
+    `${
+      isSandbox
+        ? 'https://sandbox.boletobancario.com/authorization-server/oauth/token'
+        : 'https://api.juno.com.br/authorization-server/oauth/token'
+    }`,
+    'grant_type=client_credentials',
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic: ${hash}`,
+      },
+    }
+  );
 
   if (status !== 200) throw new AppError('Cannot authorizate', status);
 
